f8c3f13cc6c3efe0bac3c90eb54568e424247f4c
Andrew Horvath <skwerl@student.fullsail.edu>
2022-01-10 17:13:14 -0600

    adding in my files. im crossing my fingers.

---
+++ src/GlobalStyles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyles } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+html {
+--color-bittersweet: rgba(255, 111, 89, 1);
+--color-darkslategray: rgba(37, 68, 65, 1);
+--color-zomp: rgba(67, 170, 139, 1);
+--color-sage: rgba(178, 176, 155, 1);
+--color-pinklace: rgba(252, 221, 242, 1);
+}
+`;
+
+export default GlobalStyle;
