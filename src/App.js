8f09e749a545729fc0da03aca651bd0f506470f2
Andrew Horvath <skwerl@student.fullsail.edu>
2022-01-12 11:09:01 -0600

    Finished up right side ads, with styling.

--- src/App.js
+++ src/App.js
@@ -12,6 +12,11 @@ import ButtonSmall from './components/buttons/ButtonSmall';
 //form
 import PostForm from './components/PostForm';
 
+//ads
+import MyAd from './components/MyAd';
+import Sprouts from './images/sprouts.jpg';
+import Tech from './images/tech-gadgets.jpg';
+
 class App extends Component {
   render() {
     return (
@@ -26,6 +31,19 @@ class App extends Component {
         <h2 style={styles.h2}>Post Form:</h2>
         <div style={styles.btnContainer}>
           <PostForm />
+        </div>
+
+        <h2 style={styles.h2}>Right Side Ads:</h2>
+        <div style={styles.btnContainer}>
+          <MyAd articleImg={Sprouts} articleAlt="This is my alt tag for brussel sprouts!!"
+          articleTitle="Brussel Sprouts"
+          articleText="Brussel sprouts are a wonderfully delicious vegetable. Eveyone should enjoy how amazing they are when roasted! a little olive oil and some salt go a long way!" />
+          
+          <MyAd articleImg={Tech} articleAlt="This is my alt tag for a bunch of techie gadgets!"
+          articleTitle="How Much Tech is Too Much Tech?"
+          articleStatement="There's no denying tech skills can give kids a leg up in the world of college and career."
+          articleText="
+          But despite the benefits, too much tech use is contributing to serious health and psychological concerns, leaving leaders with the tough task of drawing a line between access and obsession. Let's take a closer look at the impacts of too much tech use and explore 3 steps district tech leaders can take toward a more balanced approach to technology." />
         </div>
 
 
@@ -48,7 +66,6 @@ const styles = {
     display: 'flex',
     flexDirection: 'column',
     background: 'rgba(255, 235, 245, 1)',
-    height: '100vh',
     width: '75%',
     borderRadius: '5px'
   },
---
+++ src/components/myAd.js
@@ -0,0 +1,73 @@
+import React from 'react';
+
+const MyAd = props => {
+    return (
+        <article style={styles.articleAd}>
+            <img src={props.articleImg} alt={props.articleAlt} />
+            <h1 style={styles.articleTitle}>{props.articleTitle}</h1>
+            <h2 style={styles.articleStatement}>{props.articleStatement}</h2>
+            <p style={styles.articleText}>{props.articleText}
+            </p>
+            <div style={styles.buttons}>
+                <button type="submit" style={styles.cardButton}>Share</button>
+                <button type="submit" style={styles.cardButton}>More</button>
+            </div>
+        </article>
+    )
+}
+export default MyAd;
+
+const styles = {
+    articleAd: {
+        display: 'flex',
+        flexDirection: 'column',
+        width: '300px',
+        padding: '1.5%',
+        background: 'rgba(37, 68, 65, 1)',
+        borderRadius: '1rem',
+        boxShadow: '0 4px 8px 0 rgba(37, 68, 65, .2), 0 6px 20px 0 rgba(37, 68, 65, .5)',
+        textAlign: 'center',
+        margin: '1rem'
+    },
+    articleTitle: {
+        margin: '1rem 0',
+        fontSize: '2.5rem',
+        textTransform: 'uppercase',
+        color: 'rgba(247, 100, 107, 1)'
+    },
+    articleStatement: {
+       margin: '0 auto 1rem',
+        width: '90%',
+        fontSize: '1.4rem',
+        fontWeight: 'bold',
+        letterSpacing: '.2rem',
+        color: 'rgba(255, 235, 245, 1)',
+        textAlign: 'left'
+    },
+    articleText: {
+        margin: '0 auto',
+        width: '90%',
+        fontSize: '1.3rem',
+        letterSpacing: '.2rem',
+        color: 'rgba(255, 235, 245, 1)',
+        textAlign: 'left'
+    },
+    buttons: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        margin: '1rem 0rem 0 0',
+    },
+    cardButton: {
+        padding: '.5rem',
+        borderRadius: '.25rem',
+        border: 'none',
+        margin: '0 0 0 1rem',
+        fontSize: '1.3rem',
+        letterSpacing: '.1rem',
+        color: 'rgba(126, 217, 87, 1)',
+        background: 'none'
+
+
+    }
+}
