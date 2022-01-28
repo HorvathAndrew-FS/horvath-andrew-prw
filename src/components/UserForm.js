c9ad1542c8cdc56f2497a4ed531cdf728e6bcb1a
Andrew Horvath <skwerl@student.fullsail.edu>
2022-01-27 20:51:42 -0600

    update

---
+++ src/components/UserForm.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const UserForm = props => {
+    return (
+        <article key={props.id} style={styles.article}>
+            <p><img src={props.img} alt={props.alt} /></p>
+            <p>{props.username}</p>
+            <p>{props.firstName}</p>
+            <p>{props.lastName}</p>
+        </article>
+    )
+}
+
+export default UserForm;
+
+const styles = {
+    article: {
+        background: 'green',
+        color: 'white',
+        width: '50%',
+        height: '50rem',
+        fontSize: '3rem',
+    }
+}
